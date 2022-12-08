e7641b83fa2f10867d91564d8a76d0854d054170
Cameron Priest <cpriest@calpoly.edu>
2022-12-07 23:45:29 -0800

    more progress on day 7

--- day7/space.c
+++ day7/space.c
@@ -25,6 +25,7 @@ struct file {
 
 struct dir {
     char *dirName;
+    dir *parent;
     file **files;
     int numFiles;
     dir **subdirectories;
@@ -33,61 +34,36 @@ struct dir {
 
 /* global */
 dir *root;
-dir *currentDir;
+dir *currentDir = NULL;
 
-void parseCommand(char *line, char *parentDir);
-dir *parseChangeDirectoryCommand(char *directory, dir *parentDir);
+void parseCommand(char *line);
+void parseChangeDirectoryCommand(char *directory);
 void parseOutput(char *line);
 
-dir *createDirectory(char *dirName, dir *parentDir);
-
-file *createFile(char *name, int size);
-
-void addFileToDirectory(dir *d, file *f);
-
-//void printFilesystem(dir *filesystem);
-
+dir *createDirectory(char *dirName, dir *parent);
 void printDirectory(dir *directory, int numSpaces);
+void printSpaces(int numSpaces);
+
+void moveToRootDirectory(void);
+void moveUpOneDirectory(void);
+void moveDownOneDirectory(char *directory);
+
+file *createFile(char *line);
+
 
 int main(void) {
     FILE *inputFile = fopen("test-input.txt", "r");
     //FILE *inputFile = fopen("input.txt", "r");
     char line[LINE_LEN];
 
-    /* dir *root = createDirectory("/", NULL);
-    dir *currentDir = root;
-
-    // dir **filesystem = (dir **) malloc(sizeof(dir));
-    // filesystem[0] = root;
-    
-
-    dir *newDir = createDirectory("a", currentDir);
-    createDirectory("p", newDir);
-    createDirectory("b", currentDir);
-    createDirectory("c", currentDir);
-
-    printDirectory(root, 0);
-    printf("--------------------------\n"); */
-
-    //dir *filesystem = root;
-    //printFilesystem(root);
-
-    //dir *currentDir = NULL;
-
-
-    dir *parentDir = (dir *) malloc(sizeof(dir));;
+    root = createDirectory("/", NULL);
+    currentDir = root; 
 
     while (fgets(line, LINE_LEN, inputFile)) {
-        if (line[0] == '$') {
-            printf("$ ");
-            if (strncmp(&line[2], "cd", 2) == 0) {
-                printf(BLU "cd " END);
-                parentDir = parseChangeDirectoryCommand(&line[5], parentDir);
-                
-            } else if (strncmp(&line[2], "ls", 2) == 0) {
-                printf(YEL "ls\n" END);
-            }
+        line[strcspn(line, "\n")] = '\0'; /* remove trailing '\n' */
 
+        if (line[0] == '$') {
+            parseCommand(line);
         } else {
             parseOutput(line);
         }
@@ -103,78 +79,151 @@ int main(void) {
     return 0;
 }
 
-/* void parseCommand(char *line, char *parentDir) {
+void parseCommand(char *line) {
     printf("$ ");
-    if (strncmp(line, "cd", 2) == 0) {
+    if (strncmp(&line[2], "cd", 2) == 0) {
         printf(BLU "cd " END);
-        parseChangeDirectoryCommand(&line[3], parentDir);
+        parseChangeDirectoryCommand(&line[5]);
         
-    } else if (strncmp(line, "ls", 2) == 0) {
-        printf(YEL "ls\n" END);
+    } else if (strncmp(&line[2], "ls", 2) == 0) {
+        printf(BLU "ls\n" END);
     }
-} */
+}
 
-dir *parseChangeDirectoryCommand(char *directory, dir *parentDir) {
-    dir *dirToChangeTo = (dir *) malloc(sizeof(dir));
-
+void parseChangeDirectoryCommand(char *directory) {
     if (strncmp(directory, "/", 1) == 0) {
         printf("/\n");
-        root = createDirectory("/", NULL);
-        dirToChangeTo = root;
+        moveToRootDirectory();
 
     } else if (strncmp(directory, "..", 2) == 0) {
         printf("..\n");
-        dirToChangeTo = parentDir;
+        moveUpOneDirectory();
 
     } else {
-        printf("%s", directory);
-        dirToChangeTo = createDirectory(directory, parentDir);
+        printf("%s\n", directory);
+        moveDownOneDirectory(directory);
+    }
+}
+
+void moveToRootDirectory() {
+    currentDir = root;
+}
+
+void moveUpOneDirectory() {
+    currentDir = currentDir->parent;
+}
+
+void moveDownOneDirectory(char *directory) {
+    int found = 0;
+
+    for (int i = 0; i < currentDir->numSubdirectories; i++) {
+        /* printf("\t|%s|\n", currentDir->subdirectories[i]->dirName); */
+
+        if (strcmp(currentDir->subdirectories[i]->dirName, directory) == 0) {
+            currentDir = currentDir->subdirectories[i];
+            found = 1;
+            break;
+        }
     }
 
-    printf(GRN "dir to change to is %s" END, dirToChangeTo->dirName);
-    printf("--------------------------\n");
-    printDirectory(root, 0);
-    printf("--------------------------\n");
-
-    return dirToChangeTo;
+    if (!found) {
+        printf(RED "ERROR: subdirectory %s not found in parent directory %s\n" END, 
+            directory, currentDir->dirName);
+        exit(1);
+    }
 }
 
 void parseOutput(char *line) {
-    printf("  %s", line);
+    if (strncmp(line, "dir", 3) == 0) {
+        createDirectory(&line[4], currentDir);
+        printf(GRN "  dir %s\n" END, &line[4]);
+    } else {
+        file *f = createFile(line);
+        printf(YEL "  %s " END "%d\n", f->name, f->size);
+    }
 }
 
-/* void moveInOneLevel() {
+file *createFile(char *line) {
+    file *newFile = (file *) malloc(sizeof(file));
+    char *token;
 
-} */
+    token = strtok(line, " ");
+    if (token) {
+        newFile->size = atoi(token);
+    }
 
-dir *createDirectory(char *dirName, dir *parentDir) {
+    token = strtok(NULL, "\n");
+    if (token) {
+        newFile->name = (char *) malloc(sizeof(char));
+        strncpy(newFile->name, token, strlen(token) + 1);
+    }
+
+    /* printf(PUR "\tadding file [%s] to dir [%s] at %d\n" END, newFile->name, 
+        currentDir->dirName, currentDir->numFiles); */
+
+    currentDir->files = (file **) realloc(currentDir->files, sizeof(file) * (currentDir->numFiles + 1));
+    currentDir->files[currentDir->numFiles] = newFile;
+    currentDir->numFiles++;
+
+    /* printf("--------------------------\n");
+    printDirectory(currentDir, 0);
+    printf("--------------------------\n"); */
+
+    return newFile;
+}
+
+dir *createDirectory(char *dirName, dir *parent) {
     dir *newDir = (dir *) malloc(sizeof(dir));
+    
+    newDir->dirName = (char *) malloc(sizeof(char) * strlen(dirName));
+    strncpy(newDir->dirName, dirName, strlen(dirName) + 1);
 
-    newDir->dirName = dirName;
-    newDir->files = (file **) malloc(sizeof(file));;
+    newDir->parent = parent;
+    newDir->files = (file **) malloc(sizeof(file));
     newDir->numFiles = 0;
-    newDir->subdirectories = (dir **) malloc(sizeof(dir));;
+    newDir->subdirectories = (dir **) malloc(sizeof(dir));
     newDir->numSubdirectories = 0;
 
-    if (parentDir) {
-        printf(PUR "adding subdir %s to dir %s\n" END, newDir->dirName, parentDir->dirName);
-        parentDir->subdirectories[parentDir->numSubdirectories] = newDir;
-        parentDir->numSubdirectories++;
+    /* printf(BLU "new dir %s created\n" END, newDir->dirName); */
+
+    if (parent) {
+        /* printf(PUR "\tadding subdir [%s] to dir [%s] at %d\n" END, 
+            newDir->dirName, parent->dirName, parent->numSubdirectories); */
+
+        parent->subdirectories = (dir **) realloc(parent->subdirectories, 
+            sizeof(dir) * (parent->numSubdirectories + 1));
+        parent->subdirectories[parent->numSubdirectories] = newDir;
+        parent->numSubdirectories++;
+
+        /* printf("--------------------------\n");
+        printDirectory(parent, 0);
+        printf("--------------------------\n"); */
     }
 
     return newDir;
 }
 
-void printDirectory(dir *directory, int numSpaces) {
-    for (int i = 0; i < numSpaces; i++) {
-        printf(" ");
+void printDirectory(dir *d, int numSpaces) {
+    printSpaces(numSpaces);
+
+    printf("- %s (dir) [%d]\n", d->dirName, d->numSubdirectories);
+
+    if (d->numFiles != 0) {
+        for (int i = 0; i < d->numFiles; i++) {
+            printSpaces(numSpaces + 2);
+            printf("- %s (file, size=%d)\n", d->files[i]->name, d->files[i]->size);
+        }
     }
 
-    printf("- %s (dir) [%d]\n", directory->dirName, directory->numSubdirectories);
-
-    if (directory->numSubdirectories != 0) {
-        for (int i = 0; i < directory->numSubdirectories; i++) {
-            printDirectory(directory->subdirectories[i], numSpaces + 2);
+    if (d->numSubdirectories != 0) {
+        for (int i = 0; i < d->numSubdirectories; i++) {
+            printDirectory(d->subdirectories[i], numSpaces + 2);
         }
+    }
+}
+
+void printSpaces(int numSpaces) {
+    for (int i = 0; i < numSpaces; i++) {
+        printf(" ");
     }
 }
