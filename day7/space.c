5725a553f25e5bb74d2bef776fa9956b2d4810a7
Cameron Priest <cpriest@calpoly.edu>
2022-12-11 17:41:12 -0800

    day 7 done

--- day7/space.c
+++ day7/space.c
@@ -30,9 +30,10 @@ struct dir {
     int numFiles;
     dir **subdirectories;
     int numSubdirectories;
+    int totalSize;
 };
 
-/* global */
+/* globals - for code readability and ease of access from any function */
 dir *root;
 dir *currentDir = NULL;
 
@@ -44,17 +45,21 @@ dir *createDirectory(char *dirName, dir *parent);
 void printDirectory(dir *directory, int numSpaces);
 void printSpaces(int numSpaces);
 
+int calculateDirectorySizes(dir *d);
+void findSmallestDir(dir *d, int bytesToBeDeleted, int *totalSizesSum);
+
 void moveToRootDirectory(void);
 void moveUpOneDirectory(void);
 void moveDownOneDirectory(char *directory);
 
-file *createFile(char *line);
+void createFile(char *line);
 
 
 int main(void) {
-    FILE *inputFile = fopen("test-input.txt", "r");
-    //FILE *inputFile = fopen("input.txt", "r");
+    FILE *inputFile = fopen("input.txt", "r");
     char line[LINE_LEN];
+    int totalSizesSum = 0;
+    int bytesToBeDeleted = 0;
 
     root = createDirectory("/", NULL);
     currentDir = root; 
@@ -69,10 +74,17 @@ int main(void) {
         }
     }
 
-    printf("\n");
-    printf("--------------------------\n");
+    calculateDirectorySizes(root);
+    bytesToBeDeleted = 30000000 - (70000000 - root->totalSize);
+
+    printf("----------------------------------------------------\n");
     printDirectory(root, 0);
-    printf("--------------------------\n");
+    printf("----------------------------------------------------\n");
+
+    findSmallestDir(root, bytesToBeDeleted, &totalSizesSum);
+    printf("\nsum of directories with size < 100000 = %d\n\n", totalSizesSum);
+
+    /* TODO - free everything recursively */
 
     fclose(inputFile);
 
@@ -80,27 +92,17 @@ int main(void) {
 }
 
 void parseCommand(char *line) {
-    printf("$ ");
     if (strncmp(&line[2], "cd", 2) == 0) {
-        printf(BLU "cd " END);
         parseChangeDirectoryCommand(&line[5]);
-        
-    } else if (strncmp(&line[2], "ls", 2) == 0) {
-        printf(BLU "ls\n" END);
     }
 }
 
 void parseChangeDirectoryCommand(char *directory) {
     if (strncmp(directory, "/", 1) == 0) {
-        printf("/\n");
         moveToRootDirectory();
-
     } else if (strncmp(directory, "..", 2) == 0) {
-        printf("..\n");
         moveUpOneDirectory();
-
     } else {
-        printf("%s\n", directory);
         moveDownOneDirectory(directory);
     }
 }
@@ -114,36 +116,23 @@ void moveUpOneDirectory() {
 }
 
 void moveDownOneDirectory(char *directory) {
-    int found = 0;
-
     for (int i = 0; i < currentDir->numSubdirectories; i++) {
-        /* printf("\t|%s|\n", currentDir->subdirectories[i]->dirName); */
-
         if (strcmp(currentDir->subdirectories[i]->dirName, directory) == 0) {
             currentDir = currentDir->subdirectories[i];
-            found = 1;
             break;
         }
-    }
-
-    if (!found) {
-        printf(RED "ERROR: subdirectory %s not found in parent directory %s\n" END, 
-            directory, currentDir->dirName);
-        exit(1);
     }
 }
 
 void parseOutput(char *line) {
     if (strncmp(line, "dir", 3) == 0) {
         createDirectory(&line[4], currentDir);
-        printf(GRN "  dir %s\n" END, &line[4]);
     } else {
-        file *f = createFile(line);
-        printf(YEL "  %s " END "%d\n", f->name, f->size);
+        createFile(line);
     }
 }
 
-file *createFile(char *line) {
+void createFile(char *line) {
     file *newFile = (file *) malloc(sizeof(file));
     char *token;
 
@@ -158,18 +147,10 @@ file *createFile(char *line) {
         strncpy(newFile->name, token, strlen(token) + 1);
     }
 
-    /* printf(PUR "\tadding file [%s] to dir [%s] at %d\n" END, newFile->name, 
-        currentDir->dirName, currentDir->numFiles); */
-
-    currentDir->files = (file **) realloc(currentDir->files, sizeof(file) * (currentDir->numFiles + 1));
+    currentDir->files = (file **) realloc(currentDir->files, 
+        sizeof(file) * (currentDir->numFiles + 1));
     currentDir->files[currentDir->numFiles] = newFile;
     currentDir->numFiles++;
-
-    /* printf("--------------------------\n");
-    printDirectory(currentDir, 0);
-    printf("--------------------------\n"); */
-
-    return newFile;
 }
 
 dir *createDirectory(char *dirName, dir *parent) {
@@ -183,30 +164,37 @@ dir *createDirectory(char *dirName, dir *parent) {
     newDir->numFiles = 0;
     newDir->subdirectories = (dir **) malloc(sizeof(dir));
     newDir->numSubdirectories = 0;
-
-    /* printf(BLU "new dir %s created\n" END, newDir->dirName); */
+    newDir->totalSize = 0;
 
     if (parent) {
-        /* printf(PUR "\tadding subdir [%s] to dir [%s] at %d\n" END, 
-            newDir->dirName, parent->dirName, parent->numSubdirectories); */
-
         parent->subdirectories = (dir **) realloc(parent->subdirectories, 
             sizeof(dir) * (parent->numSubdirectories + 1));
         parent->subdirectories[parent->numSubdirectories] = newDir;
         parent->numSubdirectories++;
-
-        /* printf("--------------------------\n");
-        printDirectory(parent, 0);
-        printf("--------------------------\n"); */
     }
 
     return newDir;
 }
 
+int calculateDirectorySizes(dir *d) {
+    if (d->numFiles != 0) {
+        for (int i = 0; i < d->numFiles; i++) {
+            d->totalSize += d->files[i]->size;
+        }
+    }
+
+    if (d->numSubdirectories != 0) {
+        for (int i = 0; i < d->numSubdirectories; i++) {
+            d->totalSize += calculateDirectorySizes(d->subdirectories[i]);
+        }
+    }
+
+    return d->totalSize;
+}
+
 void printDirectory(dir *d, int numSpaces) {
     printSpaces(numSpaces);
-
-    printf("- %s (dir) [%d]\n", d->dirName, d->numSubdirectories);
+    printf("- %s (dir) [%d]\n", d->dirName, d->totalSize);
 
     if (d->numFiles != 0) {
         for (int i = 0; i < d->numFiles; i++) {
@@ -218,6 +206,22 @@ void printDirectory(dir *d, int numSpaces) {
     if (d->numSubdirectories != 0) {
         for (int i = 0; i < d->numSubdirectories; i++) {
             printDirectory(d->subdirectories[i], numSpaces + 2);
+        }
+    }
+}
+
+void findSmallestDir(dir *d, int bytesToBeDeleted, int *totalSizesSum) {
+    if (d->totalSize <= 100000) { /*  part 1 */
+        *totalSizesSum += d->totalSize;
+    }
+
+    if (d->totalSize >= bytesToBeDeleted) { /* part 2 */
+        printf("dir %s, size %d >= %d\n", d->dirName, d->totalSize, bytesToBeDeleted);
+    }
+
+    if (d->numSubdirectories != 0) {
+        for (int i = 0; i < d->numSubdirectories; i++) {
+            findSmallestDir(d->subdirectories[i], bytesToBeDeleted, totalSizesSum);
         }
     }
 }
